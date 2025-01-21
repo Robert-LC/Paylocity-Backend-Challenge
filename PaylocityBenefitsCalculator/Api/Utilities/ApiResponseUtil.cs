f573e57161e29b6e542cf03ab5ad1595bcef7878
Robert-LC <robert_lc1@protonmail.com>
2025-01-21 02:27:44 -0500

    Create utils

---
+++ PaylocityBenefitsCalculator/Api/Utilities/ApiResponseUtil.cs
@@ -0,0 +1,33 @@
+﻿using Api.Models;
+
+namespace Api.Utilities
+{
+    /// <summary>
+    /// Helper class with methods to streamline creating <see cref="ApiResponse{T}"./>
+    /// The methods enforce that data cannot be null if success is true and
+    /// you cannot have an error if success is true
+    /// </summary>
+    public static class ApiResponseUtil
+    {
+        public static ApiResponse<T> CreateResponse<T>(bool success, T? data = default, string? message = null, string? error = null)
+        {
+            if (success && data == null)
+            {
+                throw new ArgumentException("Data cannot be null when success is true.");
+            }
+
+            if (success && !string.IsNullOrEmpty(error))
+            {
+                throw new ArgumentException("Cannot have an error when success is true.");
+            }
+
+            return new ApiResponse<T>
+            {
+                Success = success,
+                Data = data,
+                Message = message ?? string.Empty,
+                Error = error ?? string.Empty
+            };
+        }
+    }
+}
---
+++ PaylocityBenefitsCalculator/Api/Utilities/CalculationUtil.cs
@@ -0,0 +1,22 @@
+﻿namespace Api.Utilities
+{
+    /// <summary>
+    /// Class for Calculation Utilites which may be used in multiple parts of the application
+    /// </summary>
+    public static class CalculationUtil
+    {
+        public static int CalculateAge(DateTime dateOfBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dateOfBirth.Year;
+
+            // Subtract to the right age if their birthday hasn't passed for the current year
+            if (today < dateOfBirth.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
